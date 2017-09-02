1631621d6e6a0c67c25debe400d987d9aeb0e79e
Kalle Brisland <kabc16@student.bth.se>
2017-09-02 19:53:11 +0200

    Changed/corrected flash image handling + style updates

--- view/default/header.php
+++ view/default/header.php
@@ -1,3 +1,6 @@
-<header<?= (!empty($flash) ? ' style="background-image: url(' . $this->asset($flash) . ')"' : '') ?>>
+<header<?= (!empty($flash) ? ' class="has-flash"' : '') ?>>
+<?php if (!empty($flash)) : ?>
+    <div class="flash" style="background-image: url('<?= $this->asset($flash) ?>')"></div>
+<?php endif; ?>
     <h1><?= $title ?></h1>
 </header>
