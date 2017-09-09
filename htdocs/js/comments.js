214176d95dc4039843c824c23129a50dd0de770b
Kalle Brisland <kabc16@student.bth.se>
2017-09-09 13:43:45 +0200

    Made code validate

--- htdocs/js/comments.js
+++ htdocs/js/comments.js
@@ -1,24 +1,28 @@
+/* jshint browser: true, devel: true */
+/* globals RV1 */
 (function(win, doc) {
+    "use strict";
+
     function editComment(e) {
         e.preventDefault();
         var id = e.target.getAttribute("data-id");
-        var form = document.getElementById("comment-edit-form");
+        var form = doc.getElementById("comment-edit-form");
         form.action = RV1.basePath + "comment/update/" + id;
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200) {
+                    var comment;
                     try {
-                        var comment = JSON.parse(xhr.responseText);
+                        comment = JSON.parse(xhr.responseText);
                     } catch (ex) {
                         alert("Felaktigt dataformat.");
                         return;
                     }
-                    console.log(comment);
                     form.text.innerHTML = comment.text;
                     form.text.value = comment.text;
                     form.userId.value = comment.userId;
-                    document.getElementById("comment-" + comment.id).appendChild(form);
+                    doc.getElementById("comment-" + comment.id).appendChild(form);
                     form.style.display = "block";
                 } else {
                     alert("Något gick fel.");
@@ -32,19 +36,19 @@
     function deleteComment(e) {
         e.preventDefault();
         if (confirm("Är du säker på att du vill ta bort kommentaren?")) {
-            var form = document.getElementById("comment-delete-form");
+            var form = doc.getElementById("comment-delete-form");
             form.action = RV1.basePath + "comment/delete/" + e.target.getAttribute("data-id");
             form.submit();
         }
     }
 
-    Array.prototype.forEach.call(document.getElementsByClassName("comment-edit"), function(a) {
+    Array.prototype.forEach.call(doc.getElementsByClassName("comment-edit"), function(a) {
         a.addEventListener("click", editComment);
     });
-    Array.prototype.forEach.call(document.getElementsByClassName("comment-delete"), function(a) {
+    Array.prototype.forEach.call(doc.getElementsByClassName("comment-delete"), function(a) {
         a.addEventListener("click", deleteComment);
     });
-    document.getElementById("comment-edit-cancel").addEventListener("click", function() {
-        document.getElementById("comment-edit-form").style.display = "none";
+    doc.getElementById("comment-edit-cancel").addEventListener("click", function() {
+        doc.getElementById("comment-edit-form").style.display = "none";
     });
 })(window, document);
