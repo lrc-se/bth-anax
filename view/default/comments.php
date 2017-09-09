0cc670c1a664666f65cee89743dd01198ae52a0a
Kalle Brisland <kabc16@student.bth.se>
2017-09-09 15:47:11 +0200

    Added form styles

--- view/default/comments.php
+++ view/default/comments.php
@@ -33,27 +33,50 @@
     </ol>
     <div class="comment-add">
         <h5>Skriv kommentar</h5>
-        <form action="<?= $this->url("comment/create/$contentId")?>" method="post">
+        <form class="form form-small comment-form" action="<?= $this->url("comment/create/$contentId")?>" method="post">
             <input type="hidden" name="url" value="<?= $app->request->getCurrentUrl() ?>">
 <?php if ($user) : ?>
             <input type="hidden" name="userId" value="<?= $user['id'] ?>">
+            <div class="form-control">
+                <div class="form-input">
+                    <textarea id="comment-text" name="text" rows="7" required></textarea>
+                </div>
+            </div>
 <?php else : ?>
-            <p><label>Namn: <input type="text" name="name" maxlength="100" required></label></p>
-            <p><label>E-post: <input type="email" name="email" maxlength="200"></label></p>
+            <div class="form-control">
+                <div class="form-label"><label for="comment-name">Namn:</label></div>
+                <div class="form-input"><input id="comment-name" type="text" name="name" maxlength="100" required></div>
+            </div>
+            <div class="form-control">
+                <div class="form-label"><label for="comment-email">E-post:</label></div>
+                <div class="form-input"><input id="comment-email" type="email" name="email" maxlength="200"></div>
+            </div>
+            <div class="form-control">
+                <div class="form-label"><label for="comment-text">Kommentar:</label></div>
+                <div class="form-input"><textarea id="comment-text" name="text" rows="7" required></textarea></div>
+            </div>
 <?php endif; ?>
-            <p><label>Kommentar:<br><textarea name="text" rows="5" required></textarea></label></p>
-            <p><input type="submit" value="Skicka"></p>
+            <div class="form-control">
+<?php if (!$user) : ?>
+                <div class="form-label"></div>
+<?php endif; ?>
+                <div class="form-input"><input type="submit" value="Skicka"></div>
+            </div>
         </form>
     </div>
 <?php if ($user) : ?>
-    <form id="comment-edit-form" action="" method="post" style="display: none">
+    <form id="comment-edit-form" class="form form-small comment-form" action="" method="post" style="display: none">
         <input type="hidden" name="url" value="<?= $app->request->getCurrentUrl() ?>">
         <input type="hidden" name="userId" value="">
-        <p><label>Kommentar:<br><textarea name="text" rows="5" required></textarea></label></p>
-        <p>
-            <input type="submit" value="Spara">
-            <input id="comment-edit-cancel" type="button" value="Avbryt">
-        </p>
+        <div class="form-control">
+            <div class="form-input"><textarea name="text" rows="5" required></textarea></div>
+        </div>
+        <div class="form-control">
+            <div class="form-input">
+                <input type="submit" value="Spara">
+                <input id="comment-edit-cancel" type="button" value="Avbryt">
+            </div>
+        </div>
     </form>
     <form id="comment-delete-form" action="" method="post"></form>
     <script src="<?= $this->asset('js/comments.js') ?>" async></script>
--- view/user/login.php
+++ view/user/login.php
@@ -1,5 +1,14 @@
-<form action="<?= $this->url('user/login') ?>" method="post">
-    <p><label>Användarnamn: <input type="text" name="username" required></label></p>
-    <p><label>Lösenord: <input type="password" name="password" required></label></p>
-    <p><input type="submit" value="Logga in"></p>
+<form class="form" action="<?= $this->url('user/login') ?>" method="post">
+    <div class="form-control">
+        <div class="form-label"><label for="username">Användarnamn:</label></div>
+        <div class="form-input"><input id="username" type="text" name="username" required></div>
+    </div>
+    <div class="form-control">
+        <div class="form-label"><label for="password">Lösenord:</label></div>
+        <div class="form-input"><input id="password" type="password" name="password" required></div>
+    </div>
+    <div class="form-control">
+        <div class="form-label"></div>
+        <div class="form-input"><input type="submit" value="Logga in"></div>
+    </div>
 </form>
