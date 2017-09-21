725b1b2a231f203a041ffe59a7eec3bcbbf4c62c
Kalle Brisland <kabc16@student.bth.se>
2017-09-21 18:46:44 +0200

    Added convenience methods for verifying logged-in status

--- src/Common/CommonService.php
+++ src/Common/CommonService.php
@@ -22,6 +22,45 @@ class CommonService extends BaseService
     
     
     /**
+     * Verify that there is a logged-in user.
+     *
+     * @param string    $url    Url to redirect to if verification fails.
+     * @param bool      $admin  Whether to require admin status as well.
+     *
+     * @return \LRC\User\User   Model instance of the logged-in user.
+     */
+    public function verifyUser($url = 'user/login', $admin = false)
+    {
+        $user = $this->di->user->getCurrent();
+        if (!$user) {
+            $error = 'Du måste logga in för att kunna se den begärda sidan.';
+        } elseif ($admin && !$user->admin) {
+            $error = 'Du måste logga in som administratör för att kunna se den begärda sidan.';
+        }
+        
+        if (!empty($error)) {
+            $this->di->session->set('err', $error);
+            $this->di->session->set('returnUrl', $this->di->request->getCurrentUrl());
+            $this->redirect($url);
+        }        
+        return $user;
+    }
+    
+    
+    /**
+     * Verify that there is a logged-in user with admin status.
+     *
+     * @param string    $url    Url to redirect to if verification fails.
+     *
+     * @return \LRC\User\User   Model instance of the logged-in user.
+     */
+    public function verifyAdmin($url = 'user/login')
+    {
+        return $this->verifyUser($url, true);
+    }
+    
+    
+    /**
      * Escape output.
      *
      * @param string $text  Text to escape.
