be57a8e4524cdf71c298f4c349920ceacad641c7
Kalle Brisland <kabc16@student.bth.se>
2017-09-21 18:12:29 +0200

    Added/updated user profile views

---
+++ view/user/form.php
@@ -0,0 +1,77 @@
+<?php $this->renderView('default/msgs'); ?>
+<!--<form id="<?= $form->id ?>" class="form" action="<?= $di->request->getCurrentUrl() ?>" method="post">-->
+<?= $form->form($di->request->getCurrentUrl(), 'post', ['class' => 'form']) ?>
+<?php if ($update) : ?>
+    <?= $form->input('id', 'hidden') ?>
+<?php endif; ?>
+    <div class="form-control">
+        <div class="form-label"><?= $form->label('username', 'Användarnamn:') ?></label></div>
+        <div class="form-input">
+<?php if (!$update) : ?>
+            <?= $form->textfield('username', ['required' => true, 'maxlength' => 25, 'autofocus' => true]) ?>
+<?php   if ($form->hasError('username')) : ?>
+            <div class="form-error"><?= $form->getError('username') ?></div>
+<?php   endif; ?>
+<?php else : ?>
+            <div class="form-static"><?= $di->common->esc($user->username) ?></div>
+<?php endif; ?>
+        </div>
+    </div>
+    <div class="form-control">
+        <div class="form-label"><?= $form->label('name', 'Namn:') ?></div>
+        <div class="form-input">
+            <?= $form->textfield('name', ['required' => true, 'maxlength' => 100]) ?>
+<?php if ($form->hasError('name')) : ?>
+            <div class="form-error"><?= $form->getError('name') ?></div>
+<?php endif; ?>
+        </div>
+    </div>
+    <div class="form-control">
+        <div class="form-label"><?= $form->label('password', 'Lösenord:') ?></div>
+        <div class="form-input">
+            <?= $form->textfield('password', ['minlength' => 8, 'required' => !$update, 'placeholder' => ($update ? 'Lämna blankt för att behålla nuvarande' : '')], 'password') ?>
+<?php if ($form->hasError('password')) : ?>
+            <div class="form-error"><?= $form->getError('password') ?></div>
+<?php endif; ?>
+        </div>
+    </div>
+    <div class="form-control">
+        <div class="form-label"><?= $form->label('password2', 'Upprepa lösenord:') ?></div>
+        <div class="form-input">
+            <?= $form->textfield('password2', ['minlength' => 8, 'required' => !$update], 'password') ?>
+<?php if ($form->hasError('password2')) : ?>
+            <div class="form-error"><?= $form->getError('password2') ?></div>
+<?php endif; ?>
+        </div>
+    </div>
+    <div class="form-control">
+        <div class="form-label"><?= $form->label('email', 'E-postadress:') ?></div>
+        <div class="form-input">
+            <?= $form->textfield('email', ['maxlength' => 200], 'email') ?>
+<?php if ($form->hasError('email')) : ?>
+            <div class="form-error"><?= $form->getError('email') ?></div>
+<?php endif; ?>
+        </div>
+    </div>
+<?php if ($admin) : ?>
+    <div class="form-control">
+        <div class="form-label"><?= $form->label('admin', 'Administratör:') ?></div>
+        <div class="form-input">
+<?php   if ($user->id != $admin->id) : ?>
+            <?= $form->checkbox('admin', 1) ?>
+<?php   else : ?>
+            <div class="form-static">Ja</div>
+<?php   endif; ?>
+        </div>
+    </div>
+<?php endif; ?>
+    <div class="form-control">
+        <div class="form-label"></div>
+        <div class="form-input">
+            <input type="submit" value="<?= ($update ? 'Spara' : 'Registrera') ?>">
+<?php if ($update) : ?>
+            <a class="btn btn-2" href="<?= $this->url('user/profile') ?>">Avbryt</a> 
+<?php endif; ?>
+        </div>
+    </div>
+</form>
--- view/user/index.php
+++ view/user/index.php
@@ -1,2 +1,12 @@
-<p>Du är inloggad som <strong><?= $di->common->esc($user->name) ?></strong>.</p>
-<p><a class="btn" href="<?= $this->url('user/logout') ?>">Logga ut</a></p>
+<?php $this->renderView('default/msgs'); ?>
+<p><img src="https://www.gravatar.com/avatar/<?= md5(strtolower(trim($user->email))) ?>?s=128&amp;d=retro"></p>
+<p>
+    Du är inloggad som <strong><?= $di->common->esc($user->name) ?></strong>.
+<?php if ($user->admin) : ?>
+    Ditt konto har administratörs&shy;rättigheter.
+<?php endif; ?>
+</p>
+<p>
+    <a class="btn" href="<?= $this->url('user/profile/edit/' . $user->id) ?>">Redigera profil</a>
+    <a class="btn btn-2" href="<?= $this->url('user/logout') ?>">Logga ut</a>
+</p>
