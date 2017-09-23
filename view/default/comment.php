c6b103bd84af6092b2064ff88bfbe6360806bcac
Kalle Brisland <kabc16@student.bth.se>
2017-09-23 14:59:37 +0200

    Split comment view

---
+++ view/default/comment.php
@@ -0,0 +1,31 @@
+<?php
+
+$creator = $comment->getReference('userId', $di->repository->users);
+$editor = $comment->getReference('editorId', $di->repository->users);
+
+?>
+        <li id="comment-<?= $comment->id ?>" class="comment">
+            <div class="comment-header">
+                <img src="<?= ($creator ? $creator->getGravatar(50) : (new \LRC\User\User())->getGravatar(50)) ?>">
+                <div class="comment-author">
+<?php       if ($creator && !empty($creator->email)) : ?>
+                    <a href="mailto:<?= esc($creator->email) ?>"><?= esc($creator->name) ?></a>
+<?php       else : ?>
+                    <?= ($creator ? esc($creator->name) : '(Borttagen användare)') ?>
+<?php       endif; ?>
+                </div>
+                <div class="comment-time"><?= $comment->created ?></div>
+            </div>
+            <div class="comment-text"><?= $di->textfilter->markdown(esc($comment->text)) ?></div>
+<?php       if (isset($comment->updated)) : ?>
+            <div class="comment-edited">
+                Redigerad <?= $comment->updated ?> av <?= ($editor ? esc($editor->name) : '(Borttagen användare)') ?>
+            </div>
+<?php       endif; ?>
+<?php       if ($user && ($user->admin || $comment->userId === $user->id)) : ?>
+            <div class="comment-actions">
+                <a class="comment-edit" href="#!" data-id="<?= $comment->id ?>">Redigera</a> |
+                <a class="comment-delete" href="#!" data-id="<?= $comment->id ?>">Ta bort</a>
+            </div>
+<?php       endif; ?>
+        </li>
--- view/default/comments.php
+++ view/default/comments.php
@@ -4,33 +4,7 @@
     <ol class="comment-list">
 <?php if (!empty($comments)) : ?>
 <?php   foreach ($comments as $comment) : ?>
-<?php       $creator = $comment->getReference('userId', $di->repository->users); ?>
-<?php       $editor = $comment->getReference('editorId', $di->repository->users); ?>
-        <li id="comment-<?= $comment->id ?>" class="comment">
-            <div class="comment-header">
-                <img src="<?= ($creator ? $creator->getGravatar(50) : (new \LRC\User\User())->getGravatar(50)) ?>">
-                <div class="comment-author">
-<?php       if ($creator && !empty($creator->email)) : ?>
-                    <a href="mailto:<?= esc($creator->email) ?>"><?= esc($creator->name) ?></a>
-<?php       else : ?>
-                    <?= ($creator ? esc($creator->name) : '(Borttagen användare)') ?>
-<?php       endif; ?>
-                </div>
-                <div class="comment-time"><?= $comment->created ?></div>
-            </div>
-            <div class="comment-text"><?= $di->textfilter->markdown(esc($comment->text)) ?></div>
-<?php       if (isset($comment->updated)) : ?>
-            <div class="comment-edited">
-                Redigerad <?= $comment->updated ?> av <?= ($editor ? esc($editor->name) : '(Borttagen användare)') ?>
-            </div>
-<?php       endif; ?>
-<?php       if ($user && ($user->admin || $comment->userId === $user->id)) : ?>
-            <div class="comment-actions">
-                <a class="comment-edit" href="#!" data-id="<?= $comment->id ?>">Redigera</a> |
-                <a class="comment-delete" href="#!" data-id="<?= $comment->id ?>">Ta bort</a>
-            </div>
-<?php       endif; ?>
-        </li>
+<?php       $this->renderView('default/comment', ['comment' => $comment, 'user' => $user]); ?>
 <?php   endforeach; ?>
 <?php else : ?>
         <li class="comment"><em>Inga kommentarer ännu.</em></li>
